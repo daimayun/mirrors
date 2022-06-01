101987dd78b96c124cd2abdb91aed8b36ac54187
wangben <1403375030@qq.com>
2022-06-01 11:15:29 +0800

    init

--- docker/debian.sh
+++ docker/debian.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+suffix=`date +%Y%m%d%H%m%s`
+cp /etc/apt/sources.list /etc/apt/sources.list.$suffix.backup
+rm /etc/apt/sources.list
+echo 'deb http://mirrors.aliyun.com/debian/ bullseye main non-free contrib' > /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/debian/ bullseye main non-free contrib' >> /etc/apt/sources.list
+echo 'deb http://mirrors.aliyun.com/debian-security/ bullseye-security main' >> /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/debian-security/ bullseye-security main' >> /etc/apt/sources.list
+echo 'deb http://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib' >> /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib' >> /etc/apt/sources.list
+echo 'deb http://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib' >> /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib' >> /etc/apt/sources.list
